d76044b863a9e65ade5db291d7cf7a3d17523f67
George-Stephen <yazz777stevie@gmail.com>
2020-04-24 15:22:14 +0300

    adding the objects

--- src/main/java/objects/Squad.java
+++ src/main/java/objects/Squad.java
@@ -1,14 +1,29 @@
 package objects;
 
 public class Squad{
-    int Squad_serial;
-    String Squad_Name;
-    String  Leader;
+    private int Squad_serial;
+    private String Squad_Name;
+    private String  Leader;
+    private String  Cause;
+    private int Max ;
 
-    public Squad(int squad_serial, String squad_Name, String leader) {
-        Squad_serial = squad_serial;
-        Squad_Name = squad_Name;
-        Leader = leader;
+    public Squad(int squad_serial, String squad_Name, String leader,String Cause,int Max) {
+        this.Squad_serial = squad_serial;
+        this.Squad_Name = squad_Name;
+        this.Leader = leader;
+        this.Cause = Cause;
+        this.Max = Max;
     }
 
+    public int getSquad_serial() {
+        return Squad_serial;
+    }
+
+    public String getLeader() {
+        return Leader;
+    }
+
+    public String getSquad_Name() {
+        return Squad_Name;
+    }
 }
