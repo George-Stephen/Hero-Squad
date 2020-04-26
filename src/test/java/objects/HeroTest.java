fbeec167e6f955b40ebe2b8212c1ea4c3e308408
George-Stephen <yazz777stevie@gmail.com>
2020-04-26 19:59:55 +0300

    finished

--- src/test/java/objects/HeroTest.java
+++ src/test/java/objects/HeroTest.java
@@ -8,22 +8,26 @@ public class HeroTest {
 
     @Test
     public void Hero_Present(){
-        Hero test = new Hero("Flash",25,"Superspeed", "frost");
+        Squad squad = new Squad("new","war",12);
+        Hero test = new Hero("Flash",25,"Superspeed", "frost",squad);
         assertEquals("Flash",test.getName());
     }
     @Test
     public void Hero_Age(){
-        Hero test = new Hero("Flash",25,"Superspeed", "frost");
+        Squad squad = new Squad("new","war",12);
+        Hero test = new Hero("Flash",25,"Superspeed", "frost",squad);
         assertEquals(25,test.getAge());
     }
     @Test
     public void Hero_power(){
-        Hero test = new Hero("Flash",25,"Superspeed", "frost");
+        Squad trial = new Squad("new","war",12);
+        Hero test = new Hero("Flash",25,"Superspeed", "frost",trial);
         assertEquals("Superspeed",test.getSuperpower());
     }
     @Test
     public void Hero_weakness(){
-        Hero test = new Hero("Flash",25,"Superspeed", "frost");
+        Squad tests = new Squad("soon","war",12);
+        Hero test = new Hero("Flash",25,"Superspeed", "frost",tests);
         assertEquals("frost",test.getWeakness());
     }
 
