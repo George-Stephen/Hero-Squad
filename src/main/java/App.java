635a91e198150e8217170662807337bb68ec6b73
George-Stephen <yazz777stevie@gmail.com>
2020-04-27 07:55:47 +0300

    finshing the project

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,13 +12,12 @@ public class App {
 
     public static void main(String[] args) {
         ProcessBuilder process = new ProcessBuilder();
-        Integer port;
+        int port;
         if (process.environment().get("PORT") != null) {
             port = Integer.parseInt(process.environment().get("PORT"));
         } else {
             port = 4567;
         }
-
         port(port);
 
         staticFileLocation("/public");
